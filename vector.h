0dac82262da091bc039ebb105359ae7c90e7f8bf
354169679 <354169679@qq.com>
2022-11-23 14:37:44 +0800

    f

--- vector.h
+++ vector.h
@@ -33,12 +33,12 @@ protected:
     }
 
     vector_base() : M_start(nullptr), M_finish(nullptr), M_end_of_strage(nullptr) {}
-    vector_base(const Alloc &) : M_start(nullptr), M_finish(nullptr), M_end_of_strage(nullptr){}
+    vector_base(const Alloc &) : M_start(nullptr), M_finish(nullptr), M_end_of_strage(nullptr) {}
     vector_base(size_t n, const Alloc &a)
     {
         M_start = M_allocate(n);
         M_finish = M_start;
-        M_end_of_strage = M_start + n;//内存++根据变量大小
+        M_end_of_strage = M_start + n; //内存++根据变量大小
     }
 
     ~vector_base()
@@ -48,8 +48,8 @@ protected:
     }
 };
 
-template <typename T, typename Alloc = _malloc_allocate<0>>//Alloc depends what allocator will be used
-class vector : protected vector_base<T, Alloc> // usage of template "protected"
+template <typename T, typename Alloc = _malloc_allocate<0>> // Alloc depends what allocator will be used
+class vector : protected vector_base<T, Alloc>              // usage of template "protected"
 {
 public:
     using iterator = T *;
@@ -60,82 +60,79 @@ public:
     using base_ = vector_base<T, Alloc>;
     using allocator_type = typename base_::allocator_type; // usage of "using"
 
-
-    using M_start = base_::M_start;
-    using M_finish = base_::M_finish;
-    using M_end_of_strage = base_::M_end_of_strage;
-
 public:
     vector() = default;
     vector(const Alloc &a) : base_(a) {}
-    vector(size_t n, const Alloc &a) : base_(n, a){}//allocate N of T item 
-    
-    
+    vector(size_t n, const Alloc &a) : base_(n, a) {} // allocate N of T item
+
     allocator_type get_allocator() const
     {
         return base_::get_allocator(); // usage funcation of base;
     }
 
     //踩坑记录
-    //why use "base_::M_start"
+    // why use "base_::M_start"
     // return M_start;  error!!!!
     iterator begin()
     {
-        
-        return M_start;
+
+        return base_::M_start;
         // or return this->M_start;
     }
 
     iterator end()
     {
-        return M_finish;
+        return base_::M_finish;
     }
 
     const_iterator begin() const
     {
-        return M_start;
+        return base_::M_start;
     }
 
     const_iterator end() const
     {
-        return M_finish;
+        return base_::M_finish;
     }
 
     size_type size() const
     {
-        return static_cast<size_type>(end()-begin());
+        return static_cast<size_type>(end() - begin());
     }
 
     size_type capacity() const
     {
-        return static_cast<size_type>(M_end_of_strage-begin());
+        return static_cast<size_type>(base_::M_end_of_strage - begin());
     }
 
     bool empty() const
     {
-        return end()==begin();
+        return end() == begin();
     }
 
     reference operator[](size_type index)
     {
-        return *(M_start + index);
+        return *(base_::M_start + index);
     }
 
-    reference operator[](size_type index)const
+    reference operator[](size_type index) const
     {
-        return *(M_start + index);
+        return *(base_::M_start + index);
     }
 
-    void push_back(const T& val)
+    void push_back(const T &val)
     {
-        if(end()==M_end_of_strage)
+        if (base_::M_end_of_strage != base_::M_finish)
         {
-            
+            _construct(base_::_finish, val);
+            base_::M_finish++;
+        }
+        else
+        {
         }
     }
 
 private:
-
 };
 
 #endif
